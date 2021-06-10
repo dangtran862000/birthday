d1784bd6df7353be68e9ac10ff896f687b5dfefa
Đăng La Trần Hải <s3836605@rmit.edu.vn>
2021-06-10 15:50:47 +0700

    done

---
+++ script.js
@@ -0,0 +1,64 @@
+console.log(sessionStorage.getItem("isLog"));
+console.log(document.getElementById('info-page'))
+if (sessionStorage.getItem("isLog")){
+        document.getElementById('info-page').style.display = 'block';
+        document.getElementById('login-page').style.display = 'none';
+        document.getElementById("email").value =sessionStorage.getItem('email');
+        
+    }else{
+        document.getElementById('info-page').style.display = 'none';
+        document.getElementById('login-page').style.display = 'block';
+    }
+
+function validateForm() {
+    email = document.getElementById('email-login').value;
+    password =  document.getElementById('password').value
+    if ( email=== "") {
+        setErrorFor(document.getElementById('email-login'), 'Username cannot be blank');
+    }
+    if (password=== "") {
+        setErrorFor(document.getElementById('password'), 'Password cannot be blank');
+    } else if ((password ==="11062000")){
+        if ((email==="hongnhi")) {
+            storeInfo();
+            document.getElementById('info-page').style.display = 'block';
+            document.getElementById('login-page').style.display = 'none';
+        } else {
+            setErrorFor(document.getElementById('email-login'), 'Invalid username');
+        }
+        
+        
+    }
+    else{
+        setErrorFor(document.getElementById('email-login'), 'Invalid username');
+        setErrorFor(document.getElementById('password'), 'Invalid password');;
+    }
+}
+function storeInfo(){
+    sessionStorage.setItem('email', document.getElementById('email-login').value);
+    
+    sessionStorage.setItem('isLog', true);
+    
+}
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+}
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+
+/* fucntion to display none */
+// document.getElementById('info-page').style.display =sessionStorage.getItem('display');
+// document.getElementById('login-page').style.display =sessionStorage.getItem('display-none')
+
+// document.getElementById("email").value =sessionStorage.getItem('email');
+
+function HiddenSignup(){
+    if (sessionStorage.getItem('isLog')) {
+      document.getElementById('signup').style.visibility = 'hidden';
+    }
+  }
